9176277353ff919d393e8e06304be0e2b4c46ee7
SasithaRh <ruvishansth1030@gmail.com>
2024-07-21 19:31:05 +0530

    Add layout & implement login & logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function login_admin()
+    {
+        if(!empty(Auth::check()) && Auth::user()->is_admin == 1){
+            return redirect('admin/dashboard');
+        }
+
+        return view('admin.auth.login');
+    }
+    public function auth_login_admin(Request $request)
+    {
+       $remember = !empty($request->remember) ? true:false;
+       if(Auth::attempt(['email' => $request->email, 'password' => $request->password, 'is_admin'=>1],$remember)){
+        return redirect('admin/dashboard');
+       }else{
+         return redirect()->back()->with('error','Please Enter Correct Email & Password');
+       }
+
+    }
+    public function auth_logout_admin()
+    {
+        Auth::logout();
+        return redirect('admin');
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin',[AuthController::class,'login_admin']);
+Route::post('admin',[AuthController::class,'auth_login_admin']);
+Route::get('admin/logout',[AuthController::class,'auth_logout_admin'])->name('logout');
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+
+});
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+
+});
+// Route::controller(AuthController::class)->group(function(){
+//     Route::get('/admin/logout', 'destroy' )->name('admin.logout');
+    // Route::get('/admin/profile', 'profile' )->name('admin.profile');
+    // Route::get('/edit/profile', 'editprofile' )->name('edit.profile');
+    // Route::post('/store/profile', 'stroreprofile' )->name('strore.profile');
+    // Route::get('/change/password', 'changepassword' )->name('change.password');
+    // Route::post('/update/password', 'updatepassword' )->name('update.password');
+    // Route::post('/update/password', 'updatepassword' )->name('update.password');
+
+// });
